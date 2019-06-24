359deddc846c101b1982d4c5ee85ceb5fa31feb3
Marcus Stockton <m.stockton@fargosystems.com>
2019-06-24 17:26:54 +0100

    Updated Serializer to pass id

--- items/serializers.py
+++ items/serializers.py
@@ -8,8 +8,9 @@ class ItemSerializer(serializers.HyperlinkedModelSerializer):
     reviews = ReviewSerializer(many=True, read_only=True)
     attachments = AttachmentSerializer(many=True, read_only=True)
     created_by = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    id = serializers.IntegerField()
     # parser_classes = (MultiPartParser, FormParser)
     class Meta:
         model = Item
-        #fields = ('url', 'id', 'title', 'description', 'price', 'created_date', 'created_by', 'updated_date', 'reviews')
+        #fields = ('url', 'id', 'title', 'description', 'price', 'created_date', 'created_by', 'updated_date', 'reviews', 'attachments')
         fields = "__all__"
